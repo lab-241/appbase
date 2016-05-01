9a0b6fffd35569f6e3063e0d0d5dbef7a9eb0a23
mikamboo <mike@mikangali.com>
2016-05-02 00:06:54 +0200

    TG-74 #done : FInish 'review' entity, add styled dashboard summary items [ci skip]

--- api/client/admin/admin.js
+++ api/client/admin/admin.js
@@ -6,13 +6,14 @@ function truncate(value, size) {
     return '';
   }
   if (!size) {
-    size = 20;
+    size = 30;
   }
   return value.length > size ? value.substr(0, size) + '...' : value;
 }
 
 //-- Declare a function to run when the module bootstraps
 app.config(['NgAdminConfigurationProvider', function (nga) {
+
   //-- create an admin application
   var admin = nga.application('Admin Dashboard')
              .baseApiUrl('http://localhost:3000/api/');
@@ -26,11 +27,21 @@ app.config(['NgAdminConfigurationProvider', function (nga) {
     .description('List of cities')
     .fields([
       nga.field('name').isDetailLink(true),
-      nga.field('location.lat'),
-      nga.field('location.lng')
+      nga.field('location.lat', 'float').cssClasses('col-sm-3'),
+      nga.field('location.lng', 'float').cssClasses('col-sm-3'),
     ]);
 
-    city.creationView()
+  // city.showView()
+  //   .fields([
+  //     city.listView().fields()
+  //   ]);
+
+  city.creationView()
+    .fields([
+      city.listView().fields()
+    ]);
+
+  city.editionView()
     .fields([
       city.listView().fields()
     ]);
@@ -39,14 +50,14 @@ app.config(['NgAdminConfigurationProvider', function (nga) {
   // ENTITY   : user
   // endpoint : /users/:id
   //-----------------------------------
-  var user = nga.entity('users');
-  user.listView()
-    .description('List of users')
-    .fields([
-      nga.field('username'),
-      nga.field('email')
-    ]);
-      
+  // var user = nga.entity('users');
+  // user.listView()
+  //   .description('List of users')
+  //   .fields([
+  //     nga.field('username'),
+  //     nga.field('email')
+  //   ]);
+
   //-----------------------------------
   // ENTITY   : review
   // endpoint : /reviews/:id
@@ -71,6 +82,13 @@ app.config(['NgAdminConfigurationProvider', function (nga) {
   review.showView()
     .fields([
       review.listView().fields()
+    ]);
+
+  review.creationView()
+    .fields([
+      review.listView().fields(),
+      nga.field('publisherId'),
+      nga.field('shopId')
     ]);
 
   //-----------------------------------
@@ -110,26 +128,96 @@ app.config(['NgAdminConfigurationProvider', function (nga) {
   shop.creationView().fields([
     nga.field('name'),
     nga.field('desc', 'text'),
-    nga.field('location.lat').label('Latitude'),
-    nga.field('location.lng').label('Longitude'),
+    nga.field('location.lat', 'float').label('Latitude'),
+    nga.field('location.lng', 'float').label('Longitude'),
     nga.field('cityId')
   ]);
 
   // Attach entities to app
   admin.addEntity(city);
   admin.addEntity(shop);
-  admin.addEntity(user);
+  //admin.addEntity(user);
   admin.addEntity(review);
 
-// Customize menu entries
+  //----------------------
+  // Dashboard
+  //----------------------
+  // shop.dashboardView() // customize the dashboard panel for this entity
+  //   .title('Shops')
+  //   .order(1) // display the post panel first in the dashboard
+  //   .perPage(3) // limit the panel to the 5 latest posts
+  //   .fields([
+  //     shop.listView().fields()
+  //   ]);
+  // review.dashboardView()
+  //   .title('Recent reviews')
+  //   .order(1)
+  //   .perPage(3)
+  //   .fields([
+  //     review.listView().fields()
+  //   ]);
+
+  admin.dashboard(
+    nga.dashboard()
+      .addCollection(nga.collection(nga.entity('reviews'))
+        .name('reviews')
+        .perPage(10)
+        .fields([
+          review.listView().fields()
+        ])
+      )
+      .addCollection(nga.collection(admin.getEntity('shops'))
+        .name('shops')
+        .perPage(10)
+        .fields([
+          shop.listView().fields()
+        ])
+      )
+      .addCollection(nga.collection(admin.getEntity('cities'))
+        .name('cities')
+        .perPage(10)
+        .fields([
+          city.listView().fields()
+        ])
+      )
+      .template(`
+        <div class="row dashboard-starter"></div>
+        <dashboard-summary></dashboard-summary>
+        <div class="row dashboard-content">
+          <div class="col-lg-6">
+            <div class="panel panel-green">
+              <ma-dashboard-panel collection="dashboardController.collections.reviews" entries="dashboardController.entries.reviews" datastore="dashboardController.datastore"></ma-dashboard-panel>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="panel panel-primary">
+              <ma-dashboard-panel collection="dashboardController.collections.cities" entries="dashboardController.entries.cities" datastore="dashboardController.datastore"></ma-dashboard-panel>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="panel panel-yellow">
+              <ma-dashboard-panel collection="dashboardController.collections.shops" entries="dashboardController.entries.shops" datastore="dashboardController.datastore"></ma-dashboard-panel>
+            </div>
+          </div>
+        </div>
+      `)
+  );
+
+
+  //----------------------
+  // Menu
+  //----------------------
   admin.menu(nga.menu()
-  .addChild(nga.menu(city).icon('<span class="glyphicon glyphicon-map-marker"></span>'))
-  .addChild(nga.menu(shop).icon('<span class="glyphicon glyphicon-shopping-cart"></span>'))
-  .addChild(nga.menu(user).icon('<span class="glyphicon glyphicon-user"></span>'))
-  .addChild(nga.menu(review).icon('<span class="glyphicon glyphicon-comment"></span>'))
-  .addChild(nga.menu()
-      .template(
-      '<a href="/"><i class="glyphicon glyphicon-home"></i> Exit</a>')
+    .addChild(nga.menu(city)
+      .icon('<span class="glyphicon glyphicon-map-marker"></span>'))
+    .addChild(nga.menu(shop)
+      .icon('<span class="glyphicon glyphicon-shopping-cart"></span>'))
+    // .addChild(nga.menu(user)
+    //   .icon('<span class="glyphicon glyphicon-user"></span>'))
+    .addChild(nga.menu(review)
+      .icon('<span class="glyphicon glyphicon-comment"></span>'))
+    .addChild(nga.menu()
+      .template('<a href="/"><i class="glyphicon glyphicon-home"></i> Exit</a>')
     )
   );
 
--- api/client/admin/directives.js
+++ api/client/admin/directives.js
@@ -1,14 +1,46 @@
 var app = angular.module('appbase-admin');
 
-app.directive('starRating', ['$location', function ($location) {
-    return {
-      restrict: 'E',
-      scope: {
-      	stars: '@'
-      },
-      link: function(scope, elm, attrs, ctrl) {
-      	scope.starsArray = Array.apply(null, { length: parseInt(scope.stars) }).map(Number.call, Number);
-      },
-      template: `<i ng-repeat="star in starsArray" class="glyphicon glyphicon-star"></i>`
-    };
+app.directive('starRating', function () {
+  return {
+    restrict: 'E',
+    scope: {
+    	stars: '@'
+    },
+    link: function(scope, elm, attrs, ctrl) {
+    	scope.starsArray = Array.apply(null, {
+        length: parseInt(scope.stars)
+      }).map(Number.call, Number);
+    },
+    template:
+      `<i ng-repeat="star in starsArray" class="glyphicon glyphicon-star"></i>`
+  };
+});
+
+app.directive('dashboardSummary', ['Restangular', function(Restangular){
+  return {
+    restrict: 'E',
+    scope: {},
+    controller: function($scope) {
+        $scope.stats = {};
+        Restangular
+          .all('cities')
+          .getList({sort: '["date","DESC"]'})
+          .then(cities => {
+            $scope.stats.cities = cities.data;
+          });
+        Restangular
+          .all('shops')
+          .getList({range: '[1,100]', sort: '["first_seen","DESC"]'})
+          .then(shops => {
+              $scope.stats.shops = shops.data;
+          });
+        Restangular
+          .all('reviews')
+          .getList({range: '[1,100]', sort: '["date","DESC"]'})
+          .then(reviews => {
+              $scope.stats.reviews = reviews.data;
+          });
+    },
+    templateUrl: 'tpl/dashboard-summary.html'
+  };
 }]);
